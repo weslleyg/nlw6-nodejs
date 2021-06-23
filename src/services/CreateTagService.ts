13492d78f3bd3e1124038d09a607c0a460e4d90e
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-06-23 14:50:08 -0300

    tag entity and repository

---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository, Repository } from 'typeorm';
+import { Tag } from '../entities/Tag';
+import { TagsRepository } from '../repositories/TagsRepository';
+interface ITagRequest {
+  name: string;
+}
+
+class CreateTagService {
+  private tagsRepository: Repository<Tag>
+  
+  constructor() {
+    this.tagsRepository = getCustomRepository(TagsRepository)
+  }
+
+  async execute({ name }: ITagRequest) {
+    if(!name)
+      throw new Error("Name is required.");
+    
+    const tagAlreadyExists = await this.tagsRepository.findOne({
+      name
+    });
+
+    if(tagAlreadyExists)
+      throw new Error("Name already exists.");
+
+    const tag = this.tagsRepository.create({
+      name
+    });
+    
+    await this.tagsRepository.save(tag);
+
+    return tag;
+  }
+}
+
+export { CreateTagService };
