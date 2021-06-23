6030b0ce1f468d0a224b20d5c9133a5c21781b17
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-06-23 13:24:39 -0300

    error middleware

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -4,13 +4,13 @@ import { CreateUserService } from "../services/CreateUserService";
 class CreateUserController {
   async handle(req: Request, res: Response) {
     
-    const { name, email, admin } = req.body;
+      const { name, email, admin } = req.body;
 
-    const createUserService = new CreateUserService();
+      const createUserService = new CreateUserService();
 
-    const user = await createUserService.execute({ name, email, admin });
+      const user = await createUserService.execute({ name, email, admin });
 
-    return res.json(user);
+      return res.json(user);
 
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from "express";
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from "express";
 import { router } from "./routes";
 
 import "./database";
@@ -8,5 +9,18 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return res.json(400).json({
+      error: err.message
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  });
+});
 
 app.listen(3333, () => console.log("Server on port 3333"));
