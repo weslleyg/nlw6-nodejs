1bbee36b5088954ea7a0044b46f788761becc57a
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-06-24 12:57:08 -0300

    Compliment entity and migration

--- src/entities/Tag.ts
+++ src/entities/Tag.ts
@@ -1,7 +1,7 @@
 import {Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn} from "typeorm";
 import { v4 as uuid } from "uuid";
 
-@Entity("Tags")
+@Entity("tags")
 class Tag {
   
   @PrimaryColumn()
